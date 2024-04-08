198d527652fd33dd0b8660d2170df22fae6377aa
Dean Joanidhi <deanj.development@gmail.com>
2024-04-08 06:01:45 +0300

    fixed UI issues

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from 'axios'
+import './Register.css'
 
 
 function Login() {
@@ -22,21 +23,21 @@ function Login() {
     }
 
     return (
-        <section className="">
+        <section className="Register">
 
             <div className="grid grid-cols-2 w-full bg-[#E9F7F9]">
             
-                {/*Fisrt Colun (Left side)*/}
+                {/*First Colun (Left side)*/}
                 <div class="relative w-full h-screen">
-                    <img src="/images/Register_image.png" alt="Avatar" class="w-full h-full" />
+                    <img src="/images/registerImage.png" alt="Avatar" class="w-full h-full" />
                     <div className="absolute w-[90%] h-full top-0 left-[5%] 
                                     flex justify-center items-center flex-col">
                         <label className="text-white text-5xl text-center">
-                        Breidging dreams to carres <label className=" text-[#81F9E3]">Seamlessly</label>
+                        Bridging dreams to careers <label className=" text-[#81F9E3]">Seamlessly</label>
                         </label> 
 
                         <label className="text-white text-2xl text-center mt-[20px]">
-                        Streamlime your job search with our platform- explore opportunities in <label className=" text-[#81F9E3]">minutes!</label>
+                        Streamlime your job search with our platform & explore opportunities in <label className=" text-[#81F9E3]">minutes!</label>
                         </label>         
                     </div>
                 </div>
@@ -45,7 +46,7 @@ function Login() {
                 <div>
 
                     <div className="flex justify-center items-center">
-                        <img className=" w-60 h-40" src="/images/logo-light.png" alt=""></img>    
+                        <img className=" w-60 h-40" src="/images/logoLight.png" alt=""></img>    
                     </div>
 
                     <div className="flex justify-center mt-5">
@@ -58,8 +59,8 @@ function Login() {
                                 border-2 border-[#067FB9] rounded-3xl 
                                 h-16 text-xl w-3/4 cursor-pointer mt-8
                                 inline-flex items-center justify-center">
-                                <img className="mr-2" src="/images/Google_logo.png" alt=""></img>
-                                    <span>Sign up with google</span>
+                                <img className="mr-2" src="/images/google.png" alt=""></img>
+                                    <span>Sign up with Google</span>
                                 </button>
                             </div>
                         </div>
@@ -71,31 +72,31 @@ function Login() {
 
                             {/*-------------------------- username input --------------------------*/}
                             <div>
-                                <h1 className=" font-bold">Fullname</h1>
+                                <h1 className=" font-bold mb-2">Full Name</h1>
                                 <input type="text" placeholder="Enter your username"
                                     className="block bg-[#E9F7F9] border-2 border-[#067FB9]
                                 rounded-3xl h-16 w-full text-xl mb-5
-                                focus:outline-none focus:border-[#067FB9]"
+                                focus:outline-none focus:border-[#067FB9] p-4"
                                     onChange={e => setUsername(e.target.value)} />
                             </div>
 
                             {/*-------------------------- Email input --------------------------*/}
-                            <h1 className=" font-bold">Email Adress</h1>
+                            <h1 className=" font-bold mb-2">Email Address</h1>
                             <div>
                                 <input type="text" placeholder="Enter your email adress"
                                     className="block bg-[#E9F7F9] border-2 border-[#067FB9]
                                 rounded-3xl p-1 h-16 w-full text-xl 
-                                focus:outline-none focus:border-[#067FB9] mb-5"
+                                focus:outline-none focus:border-[#067FB9] mb-5 p-4"
                                     onChange={e => setEmail(e.target.value)} />
                             </div>
 
                             {/*-------------------------- Password input --------------------------*/}
                             <div>
-                                <h1 className=" font-bold text-[#000000]/70">Password</h1>
+                                <h1 className=" font-bold text-[#000000]/70 mb-2">Password</h1>
                                 <input type="password" placeholder="Enter your password"
                                     className="block bg-[#E9F7F9] border-2 border-[#067FB9]
                                 rounded-3xl p-1 h-16 w-full text-xl
-                                focus:outline-none focus:border-[#067FB9]"
+                                focus:outline-none focus:border-[#067FB9] p-4"
                                     onChange={e => setPassword(e.target.value)} />
                                     <label className=" text-[#000000]/50">Must be at least 8 characters</label>
                             </div>
@@ -109,7 +110,7 @@ function Login() {
                                 onClick={(e) => handleSubmit(e)} />
                             </div>
 
-                            <label className="flex justify-center">Allready register?<a className=" text-[#067FB9]" href="/login">Login!</a></label>
+                            <label className="flex justify-center">Already registered? <a className=" text-[#067FB9]" href="/login">Login</a></label>
                         </form>
                     </div>
                 </div>
