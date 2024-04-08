941b4d67cd6650c20f703f2ba7c04dc6477c895f
Dean Joanidhi <deanj.development@gmail.com>
2024-04-08 04:38:38 +0300

    addition of a custom 404 page

---
+++ src/components/ErrorHandler/errorHandler.js
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+// Function Declaration
+function errorHandler() {
+
+    //const NAVIGATE = useNavigate();  
+    return(
+        <section></section>
+
+    );
+}
+
+export default errorHandler
