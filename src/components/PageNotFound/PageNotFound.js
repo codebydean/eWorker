ddddbd665ef8e893903f3adac6eecbe4064e76d0
Dean Joanidhi <deanj.development@gmail.com>
2024-04-08 04:56:58 +0300

    addition of a custom error page

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+// Function Declaration
+export default function PageNotFound() {
+
+    //const NAVIGATE = useNavigate();  
+    return(
+        <div>
+            <h1>Oops! You seem to be lost.</h1>
+        </div>
+
+    );
+}
