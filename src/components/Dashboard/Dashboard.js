a27e708972b65ecea10c03ef13fc79964838c78f
Dean Joanidhi <deanj.development@gmail.com>
2024-04-08 16:47:54 +0300

    possible fix to netlify's deploy issue

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+// Standard Imports & Dependencies
+import { userNavigate } from 'react-router-dom'
+import './Dashboard.css'
+
+
+function Dashboard() {
+    
+
+    return (
+        <section className="">
+            <div className="">
+                Welcome back
+            </div>
+        </section>
+    );
+}
+
+export default Dashboard
