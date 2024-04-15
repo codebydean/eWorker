918a22066a60425dba7cea2f50090dfb67b97809
Dean Joanidhi <deanj.development@gmail.com>
2024-04-16 02:39:41 +0300

    finalized the waiting page

--- src/components/Waiting/Waiting.js
+++ src/components/Waiting/Waiting.js
@@ -1,21 +1,27 @@
-import './Waiting.css'
+import './Waiting.css';
 
 // Function Declaration
 export default function Waiting() {
-  
-    return(
-        <div className='flex grid grid-rows-2 gap-10 h-screen PageNotFound'>
-            <div className="flex justify-center items-center">
-                <img className="h-60 w-auto" src="images/logo.png"/>
-            </div>
-            <div className="text-center text-white">
-                <h1 className="text-4xl">Oops! You seem to be <label className="text-[#067FB9]">lost.</label></h1>
-                <p className="p-4 text-lg">Looks like you've wandered off the beaten path. Let's get you back on track!</p>
-                <button className="bg-[#067FB9] text-white p-5 cursor-pointer rounded-xl">
-                    Go back to Home 🏠
-                </button>
+    return (
+        <div className='Waiting h-screen flex justify-center items-center'>
+            {/* Centered content box */}
+            <div className="max-w-lg w-full bg-white bg-opacity-50 backdrop-filter backdrop-blur-md p-10 rounded-3xl shadow-2xl text-center">
+                <img className="h-60 w-auto mx-auto mb-4" src="/images/logoLight.png" alt="Logo" />
+
+                <h1 className="text-5xl font-bold mb-4">Launching <span className='text-[#067FB9]'>Soon!</span></h1>
+                <p className="text-lg mb-6">
+                    At eWorker, we're dedicated to making a difference. Our <span className='font-bold'>open-source</span> project aims to support unemployed individuals of all backgrounds by connecting them with job opportunities and providing assistance with financial aid. <br></br>
+                    <br></br>
+                    Additionally, we strive to <span className='font-bold'>empower</span> individuals through access to educational programs, helping them enhance their skills and pursue their career goals.
+                </p>
+                <p className="text-lg mb-6">Check our Github repository frequently to stay up to date with the changes!</p>
+                
+                <div className="flex justify-center">
+                    <a href="https://github.com/codebydean/eWorker" target="_blank" className="text-gray-600 hover:text-gray-800 mr-4">
+                        <img src="/images/github.png" className='h-10'/>
+                    </a>
+                </div>
             </div>
         </div>
-
     );
 }
