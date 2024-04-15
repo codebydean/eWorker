39ea0d81c45a256794d0b3e9bee8b06d36c6d3cc
Dean Joanidhi <deanj.development@gmail.com>
2024-04-16 00:47:36 +0300

    change of nav item colors and addition of animation to profile button

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -29,40 +29,40 @@ function Dashboard() {
                     <div className='space-y-4'>
                         <nav id='main-menu' className='flex-1 space-y-2 translate-y-[-5rem]'>
                             <span className='font-bold space-y-1 space-x-2 text-lg'>Main Menu</span>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200 group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group'>
                                 <FontAwesomeIcon icon={faHouse} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Dashboard
                             </button>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200  group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200  group'>
                                 <FontAwesomeIcon icon={faCopy} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Job Listings
                             </button>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200 group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group'>
                                 <FontAwesomeIcon icon={faBook} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Training Courses</button>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200 group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group'>
                                 <FontAwesomeIcon icon={faCalendarCheck} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Work Programs</button>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200 group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group'>
                                 <FontAwesomeIcon icon={faCommentsDollar} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Financial Aid</button>
                         </nav>
 
                         <nav id='account-menu' className='flex-1 space-y-2 translate-y-[-4rem]'>
                             <span className='font-bold space-y-1 space-x-2 text-lg'>Account</span>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200 group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group'>
                                 <FontAwesomeIcon icon={faQuestion} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Get Help
                             </button>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200  group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200  group'>
                                 <FontAwesomeIcon icon={faGear} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Settings</button>
-                            <button className='flex items-center px-4 py-2.5 text-lg font-bold text-[#1CA9EE] hover:bg-[#1CA9EE] hover:text-white rounded-xl focus:bg-[#1CA9EE] focus:text-white transition-all duration-200 group'>
+                            <button className='w-full flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group'>
                                 <FontAwesomeIcon icon={faRightFromBracket} className='h-5 w-5 flex-shrik-0 mr-2' />
                                 Logout</button>
                         </nav>
                         <div className='pb-3 mt-20 fixed bottom-0'>
-                            <button className='flex items-center justify-center text-center align-center w-full px-4 py-3 text-lg font-medium -text-gray-900 hover:bg-gray-200 hover:rounded-xl hover:p-5 transition-all duration-100'>
+                            <button className='flex items-center justify-center text-center align-center w-full px-4 py-3 text-lg font-medium text-[#067FB9] hover:shadow-2xl hover:rounded-xl hover:-translate-y-2 hover:bg-[#067FB9] hover:text-white hover:w-full transition-all duration-300 hover:ease-in'>
                             <img className="flex-shrink-0 object-cover w-14 h-14 mr-5 rounded-full" src="https://landingfoliocom.imgix.net/store/collection/clarity-dashboard/images/vertical-menu/2/avatar-male.png" alt="" />
                                 Dean Joanidhi
                             </button>
