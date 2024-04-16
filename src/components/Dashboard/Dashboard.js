6a6171368de71109c70c64fa5d127ca840df277a
Dean Joanidhi <deanj.development@gmail.com>
2024-04-17 02:44:57 +0300

    removal of fontawesome imports

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,8 @@
 // Standard Imports & Dependencies
-import { useNavigate, NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './Dashboard.css'
 import Navbar from '../Navbar/Navbar'
 
-
-/* Import of Font Awesome Icons */
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHouse, faRightFromBracket, faCopy, faBook, faCalendarCheck, faCommentsDollar, faQuestion, faGear } from '@fortawesome/free-solid-svg-icons';
-
-
-
-/* Creation of a Library instance to store the variables from the font awesome imports */
-library.add(faHouse, faCopy, faBook, faCalendarCheck, faCommentsDollar, faQuestion, faGear, faRightFromBracket)
 
 
 // Initialization of the component
