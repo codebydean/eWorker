5a750e07f8e4e916294a2d98729fa70323bd0b5f
Dean Joanidhi <deanj.development@gmail.com>
2024-04-17 03:13:42 +0300

    finalization of main menu routes and imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 //Standard Imports & Dependencies
 import { BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+
+// Imports of components for initial user interaction 
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Landing from './components/LandingPage/LandingPage';
+
+// Imports for MAIN MENU components
 import Dashboard from './components/Dashboard/Dashboard';
 import JobsListings from './components/JobListings/JobsListings'
+import TrainingCourses from './components/TrainingCourses/TrainingCourses'
+import WorkPrograms from './components/WorkPrograms/WorkPrograms';
+import FinancialAid from './components/FinancialAid/FinancialAid'
+
+// Launching Soon information page for the users once they land
 import Waiting from './components/Waiting/Waiting'
+
+// Custom 404 Page
 import PageNotFound from './components/PageNotFound/PageNotFound';
+
 
 
 //React App initialization
@@ -15,12 +27,21 @@ function App() {
     <>
       <Router>
         <Routes>
+          {/* Routes for initial user interaction */}
           <Route path='/Login' element={<Login />} />
           <Route path='/Register' element={<Register />} />
           <Route path='/Home' element={<Landing />} />
+          {/* Routes for MAIN MENU components */}
           <Route path='/Dashboard' element={<Dashboard />} />
           <Route path='/JobListings' element={<JobsListings />} />
+          <Route path='/TrainingCourses' element={<TrainingCourses />} />
+          <Route path='/WorkPrograms' element={<WorkPrograms />} />
+          <Route path='/FinancialAid' element={<FinancialAid />} />
+          {/* Routes for ACCOUNT components */}
+
+          {/* Custom 404 Page  */}
           <Route path='*' element={<PageNotFound />} />
+          {/* Temporary LAUNCHIN SOON active component */}
           <Route index element={<Waiting />} />
         </Routes>
       </Router>
