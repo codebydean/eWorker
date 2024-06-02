cf9ef7ce66c48c215292d2b64fdf72d1388b4a5e
sokofreta <seitanidissokratis@gmail.com>
2024-06-02 12:14:58 +0300

    A lot of things happens Check description
    1) Create a shitty Setting page
    2)Change the model of the Users(Older version of Users in the database will not work)
    3)Fixing the bug about the picture atribute (Close E-126)
    4)Add the ability to chance the atributes of a user

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,6 +1,59 @@
+import { useContext} from "react"
+import { UserContext } from '../../components/Context/Context';
+import axios from "axios";
+
+
+
 function Settings () {
+
+    const {
+        currentUser
+      } = useContext(UserContext);
+
+      const defusername = currentUser.name
+      const defemail = currentUser.email
+      const defpassword = currentUser.password
+
+      const resetDefaultUserInfo = () =>{
+        const UserName = document.getElementById("UserName")
+        UserName.value = defusername
+
+        const Email = document.getElementById("Email")
+        Email.value = defemail
+
+        const Password = document.getElementById("Password")
+        Password.value = defpassword
+      }
+
+      const submitUserNewInfo =() =>{
+        axios.put(`http://localhost:5000/users/${defemail}`)
+      }
     return(
-        <label>SETTINGS PAGE</label>
+        <div className="Setting">
+            <div className=" flex flex-col items-center justify-center h-screen w-screen">
+                <div className="grid grid-rows-3 h-screen items-center">
+                <div className="">
+                    <label>Username</label>
+                    <input id="UserName" className="h-10 w-full border border-[#000000]" defaultValue={defusername}/>  
+                </div>
+                <div className="">
+                    <label>email</label>
+                    <input id="Email" className="h-10 w-full border border-[#000000]" defaultValue={defemail}/>    
+                </div>
+                <div className="">
+                    <label>password</label>
+                    <input id="Password" type="password" className="h-10 w-full border border-[#000000]" defaultValue={defpassword}/>    
+                </div>
+                <div>
+                    <button className="h-10 w-full border border-[#000000]" onClick={()=>{resetDefaultUserInfo()}}> Reset values</button>
+                </div>
+                <div>
+                    <button className="h-10 w-full border border-[#000000]" onClick={()=>{submitUserNewInfo()}}> Submit Changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+        
     )
 }
 export default Settings
