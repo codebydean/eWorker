b411f1e2760f39f7df4643f5a56d96bfe4405e73
Dean Joanidhi <deanj.development@gmail.com>
2024-06-05 08:05:48 +0300

    fixed the UI for the financial aid

--- src/components/FinancialAid/FinancialAidList.js
+++ src/components/FinancialAid/FinancialAidList.js
@@ -1,30 +1,22 @@
+import React from 'react';
 
-function FinancialAidList(props){
+function FinancialAidList(props) {
+    const financials = props.data;
 
-    const Financials = props.data
-
-    return(
-        <div className="w-3/4 flex flex-col justify-center items-center">
-            {/*Data at job object :
-
-            "name": "Apergia",
-            "quantity": "10",
-            "price": "0"
-            
-            */}
-               {Financials.map((financial) => (
-                    <div className="w-full h-fit min-h-[100px] p-5 mb-6 bg-blue-200 opacity-80 rounded-3xl flex justify-center flex-col" key={financial.name}>
-                       <p>name: {financial.name}</p> 
-                       <p>quantity: {financial.quantity}</p> 
-                       <p>price: {financial.price}</p> 
-
+    return (
+        <div className="w-3/4 flex flex-wrap justify-between">
+            {financials.map((financial, index) => (
+                <div key={index} className="flex flex-col items-center">
+                    <div className="w-full bg-white border-4 border-[#067FB9] rounded-2xl p-5 shadow-lg mt-2">
+                        <p className="text-xl font-bold text-[#067FB9]">{financial.name}</p>
+                        <hr className="h-1 rounded-sm bg-[#067FB9] mb-5 mt-2" />
+                        <p className="mb-5">Available Spots: {financial.quantity}</p>
+                        <p className="mb-5">Aid: {financial.price}$</p>
                     </div>
-               ))} 
+                </div>
+            ))}
         </div>
-
-
-    )
-   
+    );
 }
 
-export default FinancialAidList
+export default FinancialAidList;
