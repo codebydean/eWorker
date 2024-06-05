c7010ea43ad0850d659b22632150b30758e6f23d
Dean Joanidhi <deanj.development@gmail.com>
2024-06-05 07:59:32 +0300

    fixed the UI and added animations for the training courses

--- src/components/TrainingCourses/TrainCourcesList.js
+++ src/components/TrainingCourses/TrainCourcesList.js
@@ -1,37 +1,24 @@
+import React from 'react';
 
-function TrainCourcesList(props){
+function TrainCourcesList(props) {
+    const trainCources = props.data;
 
-    const TrainCources = props.data
-
-    return(
-        <div className="w-3/4 flex flex-col justify-center items-center">
-            {/*Data at job object :
-
-                "Title": "FRONT-END",
-                "Dates": {
-                    "Application": "Tue Jun 04 2024 22:17:04 GMT+0300 (Eastern European Summer Time)",
-                    "Duration": "2024-06-04T19:10:30.927Z",
-                    "Conducted": "2024-06-04T19:10:30.927Z"
-                },
-                "Pricing": "158",
-                "Description": "Learnig Front-end for WEB developer"
-
-            */}
-               {TrainCources.map((TrainCource) => (
-                    <div className="w-full h-fit min-h-[100px] p-5 mb-6 bg-blue-200 opacity-80 rounded-3xl flex justify-center flex-col" key={TrainCource.Title}>
-                       <p>Title: {TrainCource.Title}</p> 
-                       <p>Application: {TrainCource.Dates.Application.split("T")[0]}</p> 
-                       <p>Duration: {TrainCource.Dates.Duration.split("T")[0]}</p>
-                       <p>Conducted: {TrainCource.Dates.Conducted.split("T")[0]}</p>
-                       <p>Pricing: {TrainCource.Pricing}</p> 
-
+    return (
+        <div className="w-3/4 flex flex-wrap justify-between">
+            {trainCources.map((trainCource, index) => (
+                <div key={index} className="flex flex-col items-center">
+                    <div className="w-full bg-white border-4 border-[#067FB9] rounded-2xl p-5 shadow-lg mt-10 transition duration-500 hover:translate-y-[-.4rem]">
+                        <p className="text-xl font-bold text-[#067FB9]">Title: {trainCource.Title}</p>
+                        <hr className="h-1 rounded-sm bg-[#067FB9] mb-5 mt-2" />
+                        <p className="mb-5 text-sm">Application: {trainCource.Dates.Application.split("T")[0]}</p>
+                        <p className="mb-5 text-sm">Duration: {trainCource.Dates.Duration.split("T")[0]}</p>
+                        <p className="mb-5 text-sm">Conducted: {trainCource.Dates.Conducted.split("T")[0]}</p>
+                        <p className="mb-5 text-sm">Pricing: {trainCource.Pricing}$</p>
                     </div>
-               ))} 
+                </div>
+            ))}
         </div>
-
-
-    )
-   
+    );
 }
 
-export default TrainCourcesList
+export default TrainCourcesList;
