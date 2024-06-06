9a004c5a4a620760b1b0fe7586a0896a38264338
sokofreta <seitanidissokratis@gmail.com>
2024-06-07 00:07:15 +0300

    Add the ability to navigate trought pages to Job Listing

--- src/components/JobListings/JobsListings.js
+++ src/components/JobListings/JobsListings.js
@@ -18,9 +18,10 @@ function JobListings() {
 
 
     const [jobs,setJobs] = useState([])
+    const [page,setPage] = useState(0)
     
     function getData(){
-        axios.get(`https://www.themuse.com/api/public/jobs?page=0`)
+        axios.get(`https://www.themuse.com/api/public/jobs?page=${page}`)
         .then((response) => 
             {
             setJobs(response.data.results)
@@ -29,15 +30,25 @@ function JobListings() {
 
     useEffect(()=>{
         getData()
-    },[])
+    },[page])
+
     return (
         <main className="Dashboard flex">
             <Navbar/>
             <div className='w-full flex flex-col'>
                 <Header/>
                 <div className='w-full h-full flex flex-col items-center'>
-                    <JobCards data={jobs}/>                 
+                <div className='w-full h-full flex flex-row justify-center items-center'>
+                        <button className="flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group" onClick={()=>{if(page===0){setPage(0)}else {setPage(page-1)} }}>Previous Page</button>
+                        <button className="flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group" onClick={()=>{if(page===10){setPage(10)}else {setPage(page+1)}}}>Next Page</button>   
+                    </div>
+                    <JobCards data={jobs}/>    
+                    <div className='w-full h-full flex flex-row justify-center items-center'>
+                        <button className="flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group" onClick={()=>{if(page===0){setPage(0)}else {setPage(page-1)} }}>Previous Page</button>
+                        <button className="flex items-center px-4 py-2.5 text-lg font-bold text-[#067FB9] hover:bg-[#067FB9] hover:text-white rounded-xl focus:bg-[#067FB9] focus:text-white transition-all duration-200 group" onClick={()=>{if(page===10){setPage(10)}else {setPage(page+1)}}}>Next Page</button>   
+                    </div>
                 </div>
+                
             </div>
         </main>
     );
