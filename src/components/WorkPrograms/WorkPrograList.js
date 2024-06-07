79e30cac22248899e66a96f5edb4f3532a6cf272
Dean Joanidhi <deanj.development@gmail.com>
2024-06-07 12:04:33 +0300

    closes E-77

--- src/components/WorkPrograms/WorkPrograList.js
+++ src/components/WorkPrograms/WorkPrograList.js
@@ -1,5 +1,11 @@
+import * as React from 'react'
+import { Box, Modal } from '@mui/material'
+
 
 function WorkProgramList(props) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClosed = () => setOpen(false)
 
     const WorkPrograms = props.data
 
@@ -20,7 +26,7 @@ function WorkProgramList(props) {
             {WorkPrograms.map((WorkProgram) => (
                 <div className="w-fit h-fit min-h-[100px] p-5 mb-6 flex flex-col bg-white border-4 border-[#067FB9] rounded-2xl shadow-lg space-y-5" key={WorkProgram.name}>
                     <div className="text-lg text-[#067FB9] font-semibold">
-                        <span>CS50: Introduction to Computer Science</span>
+                        <span>Work Program A</span>
                     </div>
                     <div className="text-sm">
                         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse magna ligula, bibendum vitae urna condimentum, malesuada consectetur elit. Ut dui erat, auctor nec elementum at, tempor in neque. Donec enim ante, vestibulum vehicula sem eu, accumsan lobortis magna. Donec at lobortis arcu. Etiam id quam vehicula, tempor velit et, imperdiet eros.</span>
@@ -30,7 +36,26 @@ function WorkProgramList(props) {
                         <span className='text-[#067FB9]'>Work Address</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <button className='flex rounded-full bg-[#067FB9] text-white p-2 w-fit h-8 items-center justify-center hover:shadow-lg shadow-gray-800 transition duration-500 hover:translate-y-[-.4rem]'>See requirements!</button>
+                        <button onClick={handleOpen} className='flex rounded-full bg-[#067FB9] text-white p-2 w-fit h-8 items-center justify-center hover:shadow-lg shadow-gray-800 transition duration-500 hover:translate-y-[-.4rem]'>See requirements!</button>
+                        <Modal
+                            open={open}
+                            onClose={handleClosed}
+                            aria-labelledby="modal-modal-title"
+                            aria-describeby="modal-modal-description"
+                        >
+                            <Box className="w-[400px] -translate-x-[15%] -translate-y-[50%] absolute top-[50%] left-[50%] bg-white border-4 border-[#067FB9] p-5 shadow-lg rounded-2xl" >
+                                <div className='flex flex-col space-y-10'>
+                                    <h1 className='text-[#067FB9] font-semibold text-center'>Working Program A Papers</h1>
+                                    <span className='text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse magna ligula, bibendum vitae urna condimentum, malesuada consectetur elit.</span>
+                                    <ol className='list-decimal ml-5'>
+                                        <li>Paper 1</li>
+                                        <li>Paper 2</li>
+                                        <li>Paper 3</li>
+                                        <li>Paper 4</li>
+                                    </ol>
+                                </div>
+                            </Box>
+                        </Modal>
                     </div>
                 </div>
             ))}
